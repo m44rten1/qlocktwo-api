394ada16743749677602ba9e2d90ab200c26bcf9
maarten <m.vansteenkiste@icloud.com>
2019-10-22 15:41:08 +0200

    initial commit

---
+++ api/routes/settings.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    let rawdata = fs.readFileSync('settings.json');
+    let settings = JSON.parse(rawdata);
+
+    res.status(200).json(settings)
+});
+
+router.post('/', (req, res, next) => {
+
+    fs.writeFile('settings.json', JSON.stringify(req.body), function (err) {
+        if (err) throw err;
+
+        let rawdata = fs.readFileSync('settings.json');
+        let settings = JSON.parse(rawdata);
+
+        res.status(200).json(settings)
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const settingsRoutes = require('./api/routes/settings');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use('/settings', settingsRoutes);
+
+// Error handling
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
