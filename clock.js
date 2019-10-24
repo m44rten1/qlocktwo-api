419ea434db217ee53997624bf3fc5c80f3269d04
maarten <m.vansteenkiste@icloud.com>
2019-10-25 01:15:59 +0200

    t

--- clock.js
+++ clock.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const ws281x = require("rpi-ws281x");
+const ws281x_1 = require("rpi-ws281x");
 const convert = require("color-convert");
 const mcpadc = require("mcp-spi-adc");
 const moment = require("moment-timezone");
@@ -9,10 +10,23 @@ brightnessFilter = new Array(150).fill(0);
 
 const clock = {
   pixels: null,
+  pixel: null,  // Lonley LED
   config: {},
+  config_1: {},
   brightness: 0,
   temperature: 0,
-  init: function() {
+  initLed() {
+    this.config_1.dma = 10;
+    this.config_1.brightness = 255;
+    this.config_1.gpio = 22;
+    this.config_1.strip = "grb";
+    this.config_1.leds = 1;
+
+    ws281x_1.configure(this.config_1);
+
+    this.pixel = new Uint32Array(this.config_1.leds);
+  },
+  initLeds() {
     this.config.dma = 10;
     this.config.brightness = 255;
     this.config.gpio = 18;
@@ -22,10 +36,13 @@ const clock = {
     ws281x.configure(this.config);
 
     this.pixels = new Uint32Array(this.config.leds);
-
+  },
+  init() {
+    this.initLed();
+    this.initLeds();
     this.measurements();
   },
-  measurements: function() {
+  measurements() {
     const tempSensor = mcpadc.open(2, { speedHz: 20000 }, err => {
       if (err) throw err;
 
@@ -53,7 +70,7 @@ const clock = {
       }, 200);
     });
   },
-  render: function() {
+  render() {
     // Get settings
     let rawdata = fs.readFileSync("settings.json");
     let settings = JSON.parse(rawdata);
