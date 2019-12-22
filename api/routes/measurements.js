5c95da4e285959561ebabe89a32500cb3ac8aa6f
maarten <m.vansteenkiste@icloud.com>
2019-12-22 22:53:52 +0100

    test

--- api/routes/measurements.js
+++ api/routes/measurements.js
@@ -7,7 +7,14 @@ router.get('/temperature', (req, res, next) => {
 });
 
 router.get('/brightness', (req, res, next) => {
-    res.status(200).json(global.clock.measuredBrightness)
+    var brightness = 100 - global.clock.measuredBrightness;
+    if (brightness < 0) {
+        brightness = 0;
+    }
+    if (brightness > 100) {
+        brightness = 100;
+    }
+    res.status(200).json(brightness);
 });
 
 module.exports = router;
